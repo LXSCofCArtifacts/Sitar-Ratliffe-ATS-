34e2b8fdeb6ead80ba9facf2df180fb8baf180d9
Logan Sitar <sitarlx@g.cofc.edu>
2020-02-24 15:48:36 -0500

    Create TimeAdjust.java

---
+++ SitarRatliffe-ActivityTracker/src/com/csci360/activitytracker/TimeAdjust.java
@@ -0,0 +1,89 @@
+package com.csci360.activitytracker;
+	
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.*;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+
+public class TimeAdjust extends Application  {
+	protected int width = 54;
+	protected int height = 150;
+	protected static int sysMin = 50;
+	protected static int sysHour = 5;
+	
+	@Override
+	public void start (Stage primaryStage) {
+		try {
+			primaryStage.setTitle("TimeAdjust");
+			StackPane p = new StackPane();
+			Scene scene = new Scene(p,width,height,Color.AZURE);
+			String minTxt = Integer.toString(sysMin);
+			String hourTxt = Integer.toString(sysHour);
+			Text t1 = new Text (minTxt);
+			Text t2 = new Text (hourTxt);
+			Text t3 = new Text (":");
+			Text t4 = new Text ("\n\nAm");
+			t2.setTranslateX(-width/3);
+			t1.setTranslateX(width/3);
+			t2.setTranslateY(-height/8);
+			t1.setTranslateY(-height/8);
+			t3.setTranslateY(-height/8);
+			t4.setTranslateY(-height/7);
+			
+			
+			Button hrUp = new Button("^");
+			hrUp.setMaxSize(width/2, height/4);
+			hrUp.setTranslateY((-height/8)*3);
+			hrUp.setTranslateX(-width/4);
+			
+			Button hrDown = new Button("v");
+			hrDown.setMaxSize(width/2, height/4);
+			hrDown.setTranslateY((height/8));
+			hrDown.setTranslateX(-width/4);
+			
+			Button minUp = new Button("^");
+			minUp.setMaxSize(width/2, height/4);
+			minUp.setTranslateY((-height/8)*3);
+			minUp.setTranslateX(width/4);
+			
+			Button minDown = new Button("v");
+			minDown.setMaxSize(width/2, height/4);
+			minDown.setTranslateY((height/8));
+			minDown.setTranslateX(width/4);
+			
+			Button back = new Button("Back");
+			back.setMaxSize(width, height/4);
+			back.setTranslateY((height/8)*3);
+		
+			
+			p.getChildren().add(hrDown);
+			p.getChildren().add(hrUp);
+			p.getChildren().add(minDown);
+			p.getChildren().add(minUp);
+			p.getChildren().add(t1);
+			p.getChildren().add(t2);
+			p.getChildren().add(t3);
+			p.getChildren().add(t4);
+			p.getChildren().add(back);
+			
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
