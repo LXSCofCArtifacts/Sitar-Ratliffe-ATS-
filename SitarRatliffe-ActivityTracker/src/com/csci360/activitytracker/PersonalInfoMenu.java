fcf335e2c93945d3fb025e7fdcd499fa05448d62
Logan Sitar <sitarlx@g.cofc.edu>
2020-03-09 00:03:16 -0400

    Update PersonalInfoMenu.java

--- SitarRatliffe-ActivityTracker/src/com/csci360/activitytracker/PersonalInfoMenu.java
+++ SitarRatliffe-ActivityTracker/src/com/csci360/activitytracker/PersonalInfoMenu.java
@@ -6,18 +6,18 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 
-public class PersonalInfoMenu extends Application {
+public class PersonalInfoMenu{
 	
-	final protected int width = 54;
-	final protected int height = 160;
+	final static int width = 54;
+	final static int height = 160;
 	
-	@Override
-	public void start(Stage primaryStage) {
+	public static void PersonalInfoMenu() {
 		try {
-			primaryStage.setTitle("Personal Info Menu");
+			Stage window = new Stage();
+			window.setTitle("Personal Info Menu");
 			StackPane sp = new StackPane();
 			Scene scene = new Scene(sp,width,height);
-			Button personaldata = new Button("Personal Data Menu"); 
+			Button personaldata = new Button("Personal Info Menu 2"); 
 			Button gender = new Button("Gender");
 			Button goals = new Button("Goal Menu"); 
 			Button back = new Button("Back"); 
@@ -25,29 +25,29 @@ public class PersonalInfoMenu extends Application {
 			personaldata.setTranslateY((-height/8)*3);
 			personaldata.setMaxSize(width, height/4);
 			sp.getChildren().add(personaldata);
+			personaldata.setOnAction(e -> PersonalInfoMenu2.PersonalInfoMenu2());
 			
 			gender.setTranslateY(-height/8);
 			gender.setMaxSize(width, height/4);
 			sp.getChildren().add(gender);
+			gender.setOnAction(e -> GenderAdjust.GenderAdjust());
 			
 			goals.setTranslateY(height/8);
 			goals.setMaxSize(width, height/4);
 			sp.getChildren().add(goals);
+			goals.setOnAction(e -> GoalMenu.GoalMenu());
 			
 			back.setTranslateY((height/8)*3);
 			back.setMaxSize(width, height/4);
 			sp.getChildren().add(back);	
+			back.setOnAction(e -> window.close());
 			
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			window.setScene(scene);
+			window.show();
 			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
 	}
 }
