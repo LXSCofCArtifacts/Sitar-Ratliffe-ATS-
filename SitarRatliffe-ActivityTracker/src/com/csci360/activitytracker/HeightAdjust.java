c980717e7c14793be181ebd0dadae99cca86f1a0
Logan Sitar <sitarlx@g.cofc.edu>
2020-03-09 00:05:39 -0400

    Update HeightAdjust.java

--- SitarRatliffe-ActivityTracker/src/com/csci360/activitytracker/HeightAdjust.java
+++ SitarRatliffe-ActivityTracker/src/com/csci360/activitytracker/HeightAdjust.java
@@ -1,5 +1,5 @@
 package com.csci360.activitytracker;
-	
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -15,36 +15,42 @@ import javafx.scene.text.Text;
 
 
 public class HeightAdjust{
-	
+<<<<<<< HEAD
+
 	final static int width = 54;
 	final static int height = 150;
-	final static int tallness = 20;
 	
+=======
+	final static int width = 54;
+	final static int height = 150;
+
+>>>>>>> bb0c12451c2e0b14f4a620da671ae79629d07176
 	public static void HeightAdjust() {
 		try {
 			Stage window = new Stage();
 			window.setTitle("AgeAdjust");
 			StackPane p = new StackPane();
 			Scene scene = new Scene(p,width,height,Color.AZURE);
-			String tallnessTxt = Integer.toString(tallness);
+			String tallnessTxt = Integer.toString(StepTracker.tallness);
 			Text t = new Text (width/2, height/2, " Age:\n  "+tallnessTxt);
-			
+			t.setTranslateY(-height/8);
+
 			Button up = new Button("    ^   ");
-			up.setTranslateY(-height/4);
-			
+			up.setTranslateY((-height/8)*3);
+
 			Button down = new Button("   v   ");
-			down.setTranslateY(height/4);
-			
+			down.setTranslateY(height/8);
+
 			Button back = new Button("Back");
 			back.setMaxSize(width, height/4);
 			back.setTranslateY((height/8)*3);
 			back.setOnAction(e -> window.close());
-			
+
 			p.getChildren().add(up);
 			p.getChildren().add(down);
 			p.getChildren().add(t);
 			p.getChildren().add(back);
-			
+
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			window.setScene(scene);
 			window.show();
