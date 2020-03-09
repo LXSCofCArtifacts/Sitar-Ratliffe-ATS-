f953134f0ec667e3cead76f9a8d628bd8b1d245d
Logan Sitar <sitarlx@g.cofc.edu>
2020-03-08 23:08:10 -0400

    Update SleepTracker.java

--- SitarRatliffe-ActivityTracker/src/com/csci360/activitytracker/SleepTracker.java
+++ SitarRatliffe-ActivityTracker/src/com/csci360/activitytracker/SleepTracker.java
@@ -7,15 +7,15 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 
-public class SleepTracker extends Application {
+public class SleepTracker {
 	
-	final protected int width = 54;
-	final protected int height = 160;
+	final static int width = 54;
+	final static int height = 160;
 	
-	@Override
-	public void start(Stage primaryStage) {
+	public static void sleepTracker() {
 		try {
-			primaryStage.setTitle("Sleep Tracker");
+			Stage window = new Stage();
+			window.setTitle("Sleep Tracker");
 			StackPane sp = new StackPane();
 			Scene scene = new Scene(sp,width,height);
 			Text sleepTimer = new Text("Sleep Timer: \nHH:MM:SS"); 
@@ -30,17 +30,14 @@ public class SleepTracker extends Application {
 			back.setTranslateY((height/8)*3);
 			back.setMaxSize(width, height/4);
 			sp.getChildren().add(back);	
+			back.setOnAction(e -> window.close());
 			
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			window.setScene(scene);
+			window.show();
 			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
 	}
 }
