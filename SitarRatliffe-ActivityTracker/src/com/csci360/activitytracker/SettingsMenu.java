8a60abb7030e4c7294dd3eba4bd6182cb648f403
Logan Sitar <sitarlx@g.cofc.edu>
2020-03-08 23:08:08 -0400

    Update SettingsMenu.java

--- SitarRatliffe-ActivityTracker/src/com/csci360/activitytracker/SettingsMenu.java
+++ SitarRatliffe-ActivityTracker/src/com/csci360/activitytracker/SettingsMenu.java
@@ -6,15 +6,15 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 
-public class SettingsMenu extends Application {
+public class SettingsMenu{
 	
-	final protected int width = 54;
-	final protected int height = 160;
+	final static int width = 54;
+	final static int height = 160;
 	
-	@Override
-	public void start(Stage primaryStage) {
+	public static void settingsMenu(){
 		try {
-			primaryStage.setTitle("Settings");
+			Stage window = new Stage();
+			window.setTitle("Settings");
 			StackPane sp = new StackPane();
 			Scene scene = new Scene(sp,width,height);
 			Button timesetting = new Button("Time \nSettings"); 
@@ -37,17 +37,15 @@ public class SettingsMenu extends Application {
 			back.setTranslateY((height/8)*3);
 			back.setMaxSize(width, height/4);
 			sp.getChildren().add(back);	
+			back.setOnAction(e -> window.close());
+
 			
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			window.setScene(scene);
+			window.show();
 			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
 	}
 }
