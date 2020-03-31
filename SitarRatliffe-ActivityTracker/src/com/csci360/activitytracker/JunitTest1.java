cf650ca6bfea83d52649ac3f22597d4b938a3d5e
ryanratliffe <60327074+ryanratliffe@users.noreply.github.com>
2020-03-31 18:04:57 -0400

    junit test addition

---
+++ SitarRatliffe-ActivityTracker/src/com/csci360/activitytracker/JunitTest1.java
@@ -0,0 +1,12 @@
+package com.csci360.activitytracker;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.Assert.*;
+class JunitTest1 {
+	@Test
+	void test() {
+		Controller.main(null);
+	}
+}
